8284b6811a358c8e4719aa49efe1933a6f2a1961
Francisco Novo Rodríguez <francisco.novorodriguez@outlook.com>
2022-06-04 22:53:02 +0200

    Creado main.php como copia de main.html

---
+++ apache2/www/main.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es-ES" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style2.css">
+    <title>Main</title>
+</head>
+<body>
+    <div class="menu">
+        <nav>
+            <div class="contenedor">
+                <div class="toggle">
+                    |||
+                </div>
+                <div class="contenedor-logo">
+                    <h2>make<span>it</span>known</h2>
+                </div>
+                <ul class="links">
+                    <li id="MenuOp1" class="link"><a href="#">¿Qué está perdido?</a></li>
+                    <li id="MenuOp2" class="link"><a href="#">Mis aportaciones</a></li>
+                    <li id="MenuOp3" class="link"><a href="#" class="logout">Cerrar sesión</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <div class="cardView">
+        <div class="card">
+            <div class="face front">
+                <img src="./static/1.jpeg" alt="">
+                <h3>iPhone encontrado iPhonoñ</h3>
+            </div>
+            <div class="face back">
+                <h3>iPhone encontrado</h3>
+                <p>Se ha encontrado un iPhone en la cafetería de FNAC de A Coruña sobre las 18.30 del martes, 23 de Abril de 2022. Se ha entregado al responsable del establecimiento. Ha encontrado un iPhone en la cafetería de FNAC de A Coruña sobre las 18.30 del martes, 23 de Abril de 2022. Se ha entregado al responsable del establecimiento. Ha encontrado un iPhone en la</p>
+                <div class="link">
+                    <span>Publicado por: "usuario"</nombre></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="./menu.js"></script>
+</body>
+</html>
