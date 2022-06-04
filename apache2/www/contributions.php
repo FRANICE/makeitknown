6d3366bdfa50fb0fbca399c5e228055db0240b46
Francisco Novo Rodríguez <francisco.novorodriguez@outlook.com>
2022-06-04 22:59:09 +0200

    Creado contributions.php como copia de contributions.html

---
+++ apache2/www/contributions.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es-ES" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style2.css">
+    <title>Contributions</title>
+</head>
+<body>
+    <div class="menu">
+        <nav>
+            <div class="contenedor">
+                <div class="toggle">
+                    |||
+                </div>
+                <div class="contenedor-logo">
+                    <h2>make<span>it</span>known</h2>
+                </div>
+                <ul class="links">
+                    <li class="link"><a href="#">¿Qué está perdido?</a></li>
+                    <li class="link"><a href="#">Mis aportaciones</a></li>
+                    <li class="link"><a href="#">Cerrar sesión</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <div class="cardView">
+        <div class="card">
+            <a href="#" class="face front2 create">
+                <img src="./static/create.png" alt="">
+                <h3>Crear</h3>
+            </a>
+        </div>
+        <div class="card">
+            <div class="face front">
+                <img src="./static/1.jpeg" alt="">
+                <h3>iPhone encontrado</h3>
+            </div>
+            <div class="face back">
+                <h3 class="titleH3">iPhone encontrado</h3>
+                <p>Se ha encontrado un iPhone en la cafetería de FNAC de A Coruña sobre las 18.30 del martes, 23 de Abril de 2022. Se ha entregado al responsable del establecimiento.</p>
+                <div>
+                    <button class="deleteButton" action="deleteCard.php" method="delete"><span>Eliminar</span></button>
+                </div>
+            </div>
+        </div>
+    <div>
+    <script src="./menu.js"></script>
+</body>
+</html>
