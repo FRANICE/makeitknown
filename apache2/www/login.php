ad579ee70acd5a2c4185bfe50e3fb4f5e37a065e
Francisco Novo Rodríguez <francisco.novorodriguez@outlook.com>
2022-06-05 02:17:29 +0200

    Creado login.php como copia de login.html

---
+++ apache2/www/login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es-ES" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="contenedor-logo">
+        <h2>make<span>it</span>known</h2>
+    </div>
+    <div class="wrapper">
+        <div class="title-text">
+            <div class="title">
+                Iniciar Sesión
+            </div>
+        </div>
+        <div class="form-container">
+            <div class="form-inner">
+                <form action="#" class="login">
+                    <div class="field">
+                        <input type="email" type="text" placeholder="Correo electrónico">
+                    </div>
+                    <div class="field">
+                        <input type="password" type="text" placeholder="Contraseña">
+                    </div>
+                    <div class="pass-link">
+                        <a href="#">¿Olvidaste tu contraseña?</a>
+                    </div>
+                    <div class="field btn">
+                        <div class="btn-layer"></div>
+                        <input type="submit" value="Iniciar sesión"/>
+                    </div>
+                    <div class="signup-link">
+                        ¿No tienes cuenta? <a href="#">Regístrate</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
