e78ee6a16e058476a2aca90da69adf33abf137ae
Francisco Novo Rodríguez <francisco.novorodriguez@outlook.com>
2022-06-05 04:24:02 +0200

    Finalizado register.php

--- apache2/www/register.php
+++ apache2/www/register.php
@@ -19,32 +19,52 @@
         </div>
         <div class="form-container">
             <div class="form-inner">
-                <form action="#" class="login">
+                <form action="do_register.php" method="post" class="login">
                     <div class="field">
-                        <input name="username" id="username" type="text" placeholder="Nombre de usuario"/>
+                        <input name="username" id="username" type="text" placeholder="Nombre de usuario" required/>
                     </div>
                     <div class="field">
-                        <input name="email" id="email" type="text" placeholder="Email"/>
+                        <input name="email" id="email" type="text" placeholder="Email" required/>
                     </div>
                     <div class="field">
-                        <input name="email_confirmation" id="email_confirmation" type="text" placeholder="Confirmar email"/>
+                        <input name="email_confirmation" id="email_confirmation" type="text" placeholder="Confirmar email" onpaste="return false;" ondrop="return false;" autocomplete="off" required/>
                     </div>
                     <div class="field">
-                        <input name="password" id="password" type="password" placeholder="Contraseña"/>
+                        <input name="password" id="password" type="password" placeholder="Contraseña" onpaste="return false;" ondrop="return false;" autocomplete="off" required/>
                     </div>
                     <div class="field">
-                        <input name="password_confirmation" id="password_confirmation" type="password" placeholder="Confirmar contraseña"/>
+                        <input name="password_confirmation" id="password_confirmation" type="password" placeholder="Confirmar contraseña" onpaste="return false;" ondrop="return false;" autocomplete="off" required/>
                     </div>
                     <div class="field btn">
                         <div class="btn-layer"></div>
                         <input type="submit" value="Registrarse"/>
                     </div>
                     <div class="signup-link">
-                        ¿Ya tienes cuenta? <a href="#">Iniciar sesión</a>
+                        ¿Ya tienes cuenta? <a href="login.php">Iniciar sesión</a>
                     </div>
                 </form>
             </div>
         </div>
     </div>
+    <?php
+        //Mensaje de éxito cuando el usuario ha sido creado correctamente.
+        if(isset($_GET['success'])) {
+            //Este mensaje se mostrará si todo ha ido correctamente, es decir, success=True en el header.
+            if($_GET['success'] == TRUE) {
+                echo "<script>alert('Usuario creado correctamente. Inicia sesión clicando en el enlace de abajo.')</script>";
+            }
+        }
+        if(isset($_GET['register_failed_email'])) {
+            if($_GET['register_failed_email'] == TRUE){//Si se da el caso de que ya exista un usuario con el email introducido, se mostrará este menjage por pantalla
+                echo "<script>alert('Ya existe un usuario con el email indicado.')</script>";
+            }
+        }
+        //
+        if(isset($_GET['register_failed_unknown'])) {
+            if($_GET['register_failed_unknown'] == TRUE) {//Si sucedió un error desconocido se mostrará este mensage por pantalla
+                echo "<script>alert('Error desconozido. Inténtalo de nuevo más tarde.')</script>";
+                }
+        }
+    ?>
 </body>
 </html>
