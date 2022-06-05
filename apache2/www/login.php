fea103bb5b3a533af138345632c83205b30219ac
Francisco Novo Rodríguez <francisco.novorodriguez@outlook.com>
2022-06-05 04:26:55 +0200

    Finalizado login.php

--- apache2/www/login.php
+++ apache2/www/login.php
@@ -19,12 +19,12 @@
         </div>
         <div class="form-container">
             <div class="form-inner">
-                <form action="#" class="login">
+                <form action="do_login.php" method="post" class="login">
                     <div class="field">
-                        <input type="email" type="text" placeholder="Correo electrónico">
+                        <input type="email" name="email" type="text" placeholder="Correo electrónico" required>
                     </div>
                     <div class="field">
-                        <input type="password" type="text" placeholder="Contraseña">
+                        <input type="password" name="password" type="text" placeholder="Contraseña" required>
                     </div>
                     <div class="pass-link">
                         <a href="#">¿Olvidaste tu contraseña?</a>
@@ -34,11 +34,27 @@
                         <input type="submit" value="Iniciar sesión"/>
                     </div>
                     <div class="signup-link">
-                        ¿No tienes cuenta? <a href="#">Regístrate</a>
+                        ¿No tienes cuenta? <a href="register.php">Regístrate</a>
                     </div>
                 </form>
             </div>
         </div>
     </div>
+    <?php
+        //Error que se devuelve cuando no hay ningún usuario con el mail introducido.
+        if (isset($_GET['login_failed_email']) == 'True') {
+            echo "<script>alert('No existe ningún usuario registrado con ese email.')</script>";
+        }
+
+        //Error que se devuelve cuando la contraseña no es correcta.
+        if (isset($_GET['login_failed_password']) == 'True') {
+            echo "<script>alert('La contraseña no es correcta.')</script>";
+        }
+
+        //Error que se devuelve cuando se desconoce qué ha causado el error.
+        if (isset($_GET['login_failed_unknown']) == 'True') {
+            echo "<script>alert('Error desconozido. Inténtalo de nuevo más tarde.')</script>";
+        }
+    ?>
 </body>
 </html>
